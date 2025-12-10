bd01949adb159f0db482c879f5907247a87f98c4
SanmuganathanLathusan <sanmuganathanlathusan@gmail.com>
2025-12-10 19:58:20 +0530

    modify main .py file

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
-from expenses import add_expense, view_expenses, view_summary
+from auth import login_user, register_user
+from expenses import add_expense, view_expenses, view_summary, export_csv
 
-def menu():
+def main_menu(username):
     while True:
-        print("\n1. Add Expense\n2. View Expenses\n3. View Summary\n4. Exit")
+        print(f"\n===== EXPENSE TRACKER ({username}) =====")
+        print("1. Add Expense")
+        print("2. View All Expenses")
+        print("3. View Summary")
+        print("4. Export to CSV")
+        print("5. Logout")
+
         choice = input("Choose an option: ")
-        if choice == '1':
-            add_expense()
-        elif choice == '2':
-            view_expenses()
-        elif choice == '3':
-            view_summary()
-        elif choice == '4':
+
+        if choice == "1":
+            add_expense(username)
+        elif choice == "2":
+            view_expenses(username)
+        elif choice == "3":
+            view_summary(username)
+        elif choice == "4":
+            export_csv(username)
+        elif choice == "5":
             break
         else:
-            print("Invalid choice!")
+            print("Invalid choice.")
+
+def start():
+    while True:
+        print("\n===== WELCOME =====")
+        print("1. Login")
+        print("2. Register")
+        print("3. Exit")
+
+        choice = input("Choose an option: ")
+
+        if choice == "1":
+            username = login_user()
+            if username:
+                main_menu(username)
+
+        elif choice == "2":
+            username = register_user()
+            if username:
+                main_menu(username)
+
+        elif choice == "3":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice.")
 
 if __name__ == "__main__":
-    menu()
+    start()
